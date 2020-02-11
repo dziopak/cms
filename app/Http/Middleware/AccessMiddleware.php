8c3325062769669030206f1f03efd9f9afe03970
unknown <michal@dziopak.com>
2020-02-11 21:28:23 +0100

    Initial commit

---
+++ app/Http/Middleware/AccessMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+use Auth;
+use Role;
+use Closure;
+
+class AccessMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $permission)
+    {
+        $user = Auth::user();
+        if ($user->role_id == "99") {
+            return $next($request);
+        } else {
+            $access = unserialize($user->role->access);
+            if (in_array($permission, $access)) {
+                return $next($request);
+            } else {
+                return redirect("/");
+            }
+        }
+    }
+}
