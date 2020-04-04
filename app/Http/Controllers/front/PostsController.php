042d7e6f51043785867381b2826476aff3ef1dc6
dziopak <michal@dziopak.com>
2020-04-04 23:21:20 +0200

    Beginning of theme system - computer swap commit

---
+++ app/Http/Controllers/front/PostsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public $theme;
+
+    public function __construct()
+    {
+        $this->theme['slug'] = config('global.general.theme');
+        $this->theme['url'] = 'themes.' . $this->theme['slug'];
+    }
+
+    public function index()
+    {
+        return view($this->theme['url'] . '.posts.index', ['theme' => $this->theme]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,28 @@
 <?php
 
 use App\Http\Controllers\Admin;
-Route::get('/_debugbar/assets/stylesheets', [
-    'as' => 'debugbar-css',
-    'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@css'
-]);
+use App\Http\Controllers\Front;
 
-Route::get('/_debugbar/assets/javascript', [
-    'as' => 'debugbar-js',
-    'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@js'
-]);
 
-Route::get('/_debugbar/open', [
-    'as' => 'debugbar-open',
-    'uses' => '\Barryvdh\Debugbar\Controllers\OpenController@handler'
-]);
-
-Route::group(['prefix' => '/'], function () {
-    Route::get('/', function () {
-        Artisan::call('storage:link');
-    });
+//Front-office routes
+Route::group(['as' => 'front.'], function () {
+    Route::get('/', 'front\PostsController@index')->name('posts.index');
 });
 
-Route::get('/locale', LocaleController::class)->name('locale');
 
-//Backoffice routes
+//Back-office routes
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'access:ADMIN_VIEW'], function () {
-    
+
     Route::get('/', 'admin\DashboardController@index')->name('dashboard.index');
     Route::post('/dashboard', 'admin\DashboardController@update')->name('dashboard.update');
     Route::get('/dashboard/widget', 'admin\DashboardController@getWidget')->name('dashboard.getwidget');
-    
-    Route::get('/clear-cache', function() {
+
+    Route::get('/clear-cache', function () {
         Artisan::call('cache:clear');
         return redirect(route('admin.dashboard.index'));
     });
 
-    Route::get('/menus', function() {
+    Route::get('/menus', function () {
         return view('admin.menus.index');
     })->name('menus.index');
 
@@ -47,18 +33,17 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'access:ADM
     require base_path('routes/web/admin/users.php');
 
     //POSTS ROUTES
-    require base_path('routes/web/admin/posts.php');    
-    
+    require base_path('routes/web/admin/posts.php');
+
     //PAGES ROUTES
-    require base_path('routes/web/admin/pages.php');    
-    
+    require base_path('routes/web/admin/pages.php');
+
     //SETTINGS ROUTES
-    require base_path('routes/web/admin/settings.php');    
-    
+    require base_path('routes/web/admin/settings.php');
+
     //VENDOR ROUTES
-    require base_path('routes/web/admin/vendor/filemanager.php');    
+    require base_path('routes/web/admin/vendor/filemanager.php');
 });
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/locale', LocaleController::class)->name('locale');
